5b85310869514c414047daeadbca9050bafe32a9
Niyam Bhat <niyambhat@192-168-1-101.tpgi.com.au>
2023-05-24 11:23:36 +0800

    first commit

---
+++ Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace IntroCS
+{
+	public class Person
+	{
+        public void Test0()
+        {
+            Console.WriteLine("public method");
+        }
+        private void Test1()
+        {
+            Console.WriteLine("private method");
+        }
+        internal void Test2()
+        {
+            Console.WriteLine("internal method");
+        }
+        protected void Test3()
+        {
+            Console.WriteLine("protected method");
+        }
+        protected internal void Test4()
+        {
+            Console.WriteLine("protected internal method");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntroCS
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is main method");
+            Person niyam = new Person();
+            niyam.DisplayInfo();
+            Console.WriteLine(niyam.name);
+
+        }
+
+    }
+}
